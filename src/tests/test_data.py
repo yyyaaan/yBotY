ad14a165b858c6654b19bf6dbe0a4f2c151b8b73
Yan Pan <yan@yan.fi>
2023-08-07 13:53:33 +0300

    correction in unittests

--- src/tests/test_data.py
+++ src/tests/test_data.py
@@ -3,12 +3,12 @@ from fastapi.testclient import TestClient
 
 from main import app
 from dataAPIs.StatFinnPaavo import StatFinPaavo
-from prompts.VectorStorage import VectorStorage
 
 client = TestClient(app)
+
 
 def test_paavo():
     """fetch Paavo data from tilastokeskus and create a collection"""
     df = StatFinPaavo().get_data()
-    df.to_csv("/mnt/shared/upload/tmptmp.csv")
-
+    assert df.shape[0] > 0
+    assert df.shape[1] > 0
